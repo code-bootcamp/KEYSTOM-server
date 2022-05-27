9fa30ed07ac87f726eec810f016cfeee2c36da5d
antipiebse <antipiebse@gmail.com>
2022-05-28 01:42:06 +0900

    [feat] 성민 쿠키 삭제 적용 final

--- src/apis/auth/auth.resolver.ts
+++ src/apis/auth/auth.resolver.ts
@@ -92,9 +92,9 @@ export class AuthResolver {
 
       if (!accessToken) throw new BadRequestException('잘못된 요청');
       if (!refreshToken) throw new BadRequestException('잘못된 요청');
-      context.res.clearCookie('refreshToken', {
-        maxAge: 0,
-      });
+      // context.res.clearCookie('refreshToken', {
+      //   maxAge: 0,
+      // });
       context.res.clearCookie('refreshToken', {
         path: '/',
         domain: '.antipiebse.shop',
