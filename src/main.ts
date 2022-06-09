071c2cb65cc5b0e2296405a6c4c9192f24579db9
antipiebse <antipiebse@gmail.com>
2022-06-09 13:42:25 +0900

    [fix] 성민 배포 후 cors설정

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { graphqlUploadExpress } from 'graphql-upload';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://keystom.site:443',
+    origin: 'https://keystom.site',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
